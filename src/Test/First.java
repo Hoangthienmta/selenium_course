2868163a1fc1d14174a570a817afdfbd4ed0156f
Hoangthienmta <Hoangthiend10xdctd@gmail.com>
2023-06-13 14:42:34 +0700

    First Commit

---
+++ src/Test/First.java
@@ -0,0 +1,25 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+
+	public First() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver", "G:\\Chrome_Driver\\chromedriver_win32(1)\\chromedriver.exe");
+		 
+		 WebDriver driver = new ChromeDriver();
+		 
+		 driver.navigate().to("https://anhtester.com");
+
+	        //3 - Lấy Title và in ra console
+	        
+	      System.out.println(driver.getTitle());
+
+	}
+
+}
